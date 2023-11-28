5201b0db513eaf0823a5dc023458884d70955b58
Yoshidak-ta <bsk.gooserock@gmail.com>
2023-11-28 10:59:06 +0900

    課題提出

---
+++ src/kadai_014/CarExec_Chapter14.java
@@ -0,0 +1,12 @@
+package kadai_014;
+
+public class CarExec_Chapter14 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+       Car_Chapter14 Car = new Car_Chapter14(1, 10);
+       
+       Car.gearChange(5);
+	}
+
+}
---
+++ src/kadai_014/Car_Chapter14.java
@@ -0,0 +1,27 @@
+package kadai_014;
+
+public class Car_Chapter14 {
+//	 フィールド
+	  private int gear = 0; 
+	  private int speed = 0;
+	  
+// コントラクタ
+   public Car_Chapter14(int gear, int speed) {
+	  this.gear = gear;
+	  this.speed = speed;
+ }
+ 
+  
+// メソッド
+   public void gearChange(int afterGear) {
+	  System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました");
+       int Speed = this.speed * afterGear;
+       if(Speed > 50) {
+    	   System.out.println("速度は時速10kmです");
+       } else if(Speed <= 50) {
+    	   System.out.println("速度は時速" + Speed + "kmです");
+       }
+   }
+   
+   
+}
